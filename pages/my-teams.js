e141521203e3134475e493d278750391c2aa6c26
Hannah-Frost <h.7frost@gmail.com>
2021-04-15 23:40:07 +0100

    Add side nav + top welcome

---
+++ pages/my-teams.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import { Flex } from "@chakra-ui/react";
+
+export default function NewTeam() {
+  return (
+    <Flex>
+      <Head>
+        <title>New Team</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </Flex>
+  );
+}
---
+++ src/components/TopWelcome.tsx
@@ -0,0 +1,55 @@
+import { Flex, Heading, Image, Box, Text } from "@chakra-ui/react";
+
+const HeadingContainer = () => (
+  <Flex alignItems="center" m={8} zIndex={99}>
+    <Image height={16} src="/images/welcome_logo.svg" />
+  </Flex>
+);
+
+export const TopWelcome = ({ element }: { element: any }) => {
+  return (
+    <Flex
+      flexDir="column"
+      ref={element}
+      height={64}
+      backgroundImage="linear-gradient(#92D0F3, #D3E8F4)"
+      alignItems="center"
+      width="100%"
+    >
+      <Flex width="100%" justifyContent="space-between">
+        <HeadingContainer />
+        <Flex alignItems="center" flexDir="row">
+          <Heading>Placeholder</Heading>
+        </Flex>
+      </Flex>
+      <Image
+        position="absolute"
+        left={0}
+        src="/images/welcome_bg.svg"
+        height={64}
+      />
+      <Image
+        position="absolute"
+        src="/images/mareep_run_1.png"
+        _hover={{ src: "/images/mareep_run_2.svg" }}
+        top={115}
+        left={470}
+        height={32}
+      />
+      <Image
+        position="absolute"
+        height={10}
+        left={320}
+        top={215}
+        src="/images/welcome_grass.svg"
+      />
+      <Image
+        position="absolute"
+        height={8}
+        left={140}
+        top={198}
+        src="/images/welcome_grass.svg"
+      />
+    </Flex>
+  );
+};
